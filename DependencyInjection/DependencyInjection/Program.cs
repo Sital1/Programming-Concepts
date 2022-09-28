d939b6d602d9e9ed31894fc0331ddcc2e25a1240
Sital Sitoula <heavywater221@gmail.com>
2022-09-28 14:48:26 -0500

    Creating Objext using reflection

--- DependencyInjection/DependencyInjection/Program.cs
+++ DependencyInjection/DependencyInjection/Program.cs
@@ -6,8 +6,15 @@ public class Program
     {   
         // manually instantiating classes
         // we are trying to get rid of new keyword
-        var service = new HelloService();
-        var consumer = new ServiceConsumer(service);
+        // var service = new HelloService();
+        // var consumer = new ServiceConsumer(service);
+        
+        
+        // using Reflection API to instantiate an object
+        // manual but the code is instantiating at run time based on knowledge about it self.
+        var service = (HelloService)Activator.CreateInstance(typeof(HelloService));
+        var consumer = (ServiceConsumer)Activator.CreateInstance(typeof(ServiceConsumer),service);
+        
         service.Print();
         consumer.Print();
     }
