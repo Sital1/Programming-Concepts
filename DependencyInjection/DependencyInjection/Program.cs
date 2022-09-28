184a5fc0304f85acd51b64d02ab0aa8b2ab1ca2d
Sital Sitoula <heavywater221@gmail.com>
2022-09-28 14:43:16 -0500

    creating service with new keyword

---
+++ DependencyInjection/DependencyInjection/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace DependencyInjection;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {   
+        // manually instantiating classes
+        // we are trying to get rid of new keyword
+        var service = new HelloService();
+        var consumer = new ServiceConsumer(service);
+        service.Print();
+        consumer.Print();
+    }
+}
+
+public class ServiceConsumer
+{
+    private readonly HelloService _helloService;
+
+    public ServiceConsumer(HelloService helloService)
+    {
+        _helloService = helloService;
+    }
+
+    public void Print()
+    {
+        _helloService.Print();
+    }
+}
+
+public class HelloService
+{
+    public void Print()
+    {
+        Console.WriteLine("hello world");
+    }
+}
