963d19a2e86dc1a977c46a8a0610afdc49ef3624
Sital Sitoula <heavywater221@gmail.com>
2022-10-08 20:31:20 -0500

    Delegates, Func, Action, Closure.

---
+++ Delegatess/Lambdas/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Lambdas;
+
+public class Program
+{   
+    public static void Main(string[] args)
+    {
+
+        // On high level, lambda is an anonymous function. It is separate from Delegate, Action, Func. Delegate represent function.
+        // Delegate is a container where function goes
+        GetNumber getN = () => 5;
+        Console.WriteLine(getN());
+        
+        // closure. Adding value to scope of the obj where the function is being performed.
+        // 
+        int i = 5;
+        GetNumber getNC = () => 5 + i;
+        Console.WriteLine(getN());
+    }
+
+    delegate int GetNumber();
+}
---
+++ Delegatess/README.md
@@ -0,0 +1,6 @@
+﻿## Delegates, Func, Actions
+
+Functional Programming paradigm in which we treat function as values. Storing functions as a normal variable.
+
+
+A signature of the function is the return type and parameter. Same signature allows us to align functions.
